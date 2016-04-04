51ef5fce7299789869241e5e90fe1d5d55c1ec36
edude <edude@localhost.localdomain>
2016-04-04 03:08:30 -0400

    First face recognition push. PCA example

---
+++ example_pca.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('/usr/local/lib/python2.7/dist-packages')
+import facerecognition.pca as pcalib
+import facerecognition.facerecutil as util
+import matplotlib.cm as cm
+
+
+# read images
+[X, y] = util.read_training_images("./facerecognition/faces")
+# perform a full pca
+[D, W, mu] = pcalib.pca(util.asRowMatrix(X), y)
+print(X)
+# turn the first (at most ) 16 eigenvectors into grayscale
+# images ( note : eigenvectors are stored by column !)
+E = []
+for i in xrange(min(len(X), 20)):
+    e = W[:, i].reshape(X[0].shape)
+    E.append(util.normalize(e, 0, 255))
+# plot them and store the plot to " python_eigenfaces.pdf"
+pcalib.subplot(title=" Eigenfaces AT&T Facedatabase ", images=E, rows=5, cols=4,
+        sptitle="Eigenface ", colormap=cm.jet,
+        filename="pca/python_pca_eigenfaces.png")
+
+
+# read images
+[X, y] = util.read_training_images("./facerecognition/faces")
+# perform a full pca
+[D, W, mu] = pcalib.pca(util.asRowMatrix(X), y)
+print(X)
+
+# reconstruction steps
+steps = [i for i in xrange(10, min(len(X), 320), 20)]
+E = []
+for i in xrange(min(len(steps), 16)):
+    numEvs = steps[i]
+    P = pcalib.project(W[:, 0: numEvs], X[0].reshape(1, -1), mu)
+    R = pcalib.reconstruct(W[:, 0: numEvs], P, mu)
+    # reshape and append to plots
+    R = R.reshape(X[0].shape)
+    E.append(util.normalize(R, 0, 255))
+pcalib.subplot(title=" Reconstruction AT&T Facedatabase ", images=E, rows=4, cols=4,
+        sptitle="Eigenvectors ", sptitles=steps, colormap=cm.gray,
+        filename="pca/python_pca_reconstruction.png")
---
+++ facerecognition/pca.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append('/usr/local/lib/python2.7/dist-packages')
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+def pca(X, y, num_components=0):
+    [n, d] = X.shape
+    if (num_components <= 0) or (num_components > n):
+        num_components = n
+    mu = X.mean(axis=0)
+    X = X - mu
+    if n > d:
+        C = np.dot(X.T, X)
+        [eigenvalues, eigenvectors] = np.linalg.eigh(C)
+    else:
+        C = np.dot(X, X.T)
+        [eigenvalues, eigenvectors] = np.linalg.eigh(C)
+        eigenvectors = np.dot(X.T, eigenvectors)
+        for i in xrange(n):
+            eigenvectors[:, i] = eigenvectors[
+                :, i]/np.linalg.norm(eigenvectors[:, i])
+    # or simply perform an economy size decomposition
+    # eigenvectors, eigenvalues, variance = np.linalg.svd (X.T, full_matrices = False )
+    # sort eigenvectors descending by their eigenvalue
+    idx = np.argsort(- eigenvalues)
+    eigenvalues = eigenvalues[idx]
+    eigenvectors = eigenvectors[:, idx]
+    # select only num_components
+    eigenvalues = eigenvalues[0: num_components].copy()
+    eigenvectors = eigenvectors[:, 0: num_components].copy()
+    return [eigenvalues, eigenvectors, mu]
+
+
+def project(W, X, mu=None):
+    if mu is None:
+        return np.dot(X, W)
+    return np.dot(X - mu, W)
+
+
+def reconstruct(W, Y, mu=None):
+    if mu is None:
+        return np.dot(Y, W.T)
+    return np.dot(Y, W.T) + mu
+
+
+def subplot(title, images, rows, cols, sptitle=" subplot ", sptitles=[],
+            colormap=cm.gray, ticks_visible=True, filename=None):
+    fig = plt.figure()
+    # main title
+    fig.text(.5, .95, title, horizontalalignment='center')
+    for i in xrange(len(images)):
+        ax0 = fig.add_subplot(rows, cols, (i + 1))
+        plt.setp(ax0.get_xticklabels(), visible=False)
+        plt.setp(ax0.get_yticklabels(), visible=False)
+        if len(sptitles) == len(images):
+            plt.title("%s #%s" % (sptitle, str(sptitles[i])))
+        else:
+            plt.title("%s #%d" % (sptitle, (i + 1)))
+            plt.imshow(np.asarray(images[i]), cmap=colormap)
+    if filename is None:
+        plt.show()
+    else:
+        fig.savefig(filename)

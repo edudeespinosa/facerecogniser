ac103afe5db1f84b17c06b8e006c0558c69bf64b
edude <edude@localhost.localdomain>
2016-04-04 03:04:38 -0400

    face tracking implementation

--- objecttracking/objectTracking.py
+++ objecttracking/objectTracking.py
@@ -1,7 +1,7 @@
 import numpy as np
-import argparse
 import cv2
-
+import facedetector.violajones as vj
+import facedetector.facedetector as fd
 roiPts = []
 
 class Tracker:
@@ -16,6 +16,8 @@ class Tracker:
         self.objectToTrack = None
         self.face = face
         self.roiHist = None
+        self.termCrit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
     # Seleccionar area de interes en el self.frame deseado.
     def boxSelection(self, event, x, y, flags, param):
         global roiPts
@@ -26,28 +28,28 @@ class Tracker:
             cv2.circle(self.frame, RoiPoint, 10, (255, 255, 255), 2)
             cv2.imshow("frame", self.frame)
     def trackObject(self):
+        global roiPts
         if self.type == 'camera':
             camera = cv2.VideoCapture(0)
         else:
             camera = cv2.VideoCapture(self.source)
         cv2.namedWindow("frame")
         cv2.setMouseCallback("frame", self.boxSelection)
+        i = 0
 
-        termCrit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-
-        while True:
+        while camera.read():
             # seleccionar self.frame
             (selected, self.frame) = camera.read()
 
-
             # existe video?
             if not selected:
+                print("really?")
                 break
-
 
             # aqui hay dos opciones:
             #   ya hay objeto para seguir. Es decir, ya hay area de interese seleccionada
             #   No hay objeto seleccionado para seguir. Se debe entrar al modo de seleccion
+
 
             # ya hay objeto?
             if self.objectToTrack is not None:
@@ -55,22 +57,29 @@ class Tracker:
                 hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
                 backProj = cv2.calcBackProject([hsv], [0], self.roiHist, [0, 180], 1)
 
-                # aplicar camshift
+                # aplicar camshift/meanshift
                 if self.face==None:
-                    (r, self.objectToTrack) = cv2.meanShift(backProj, self.objectToTrack, termCrit)
-                    x,y,w,h = self.objectToTrack
-                    cv2.rectangle(self.frame, (x,y), (x+w,y+h), 255,2)
+                    color = (255,0,0)
+                    (r, self.objectToTrack) = cv2.meanShift(backProj, self.objectToTrack, self.termCrit)
                 else:
-                    (r, self.objectToTrack) = cv2.CamShift(backProj, self.objectToTrack, termCrit)
-                    pts = np.int0(cv2.cv.BoxPoints(r))
-                    cv2.polylines(self.frame, [pts], True, (0, 255, 0), 2)
+                    color = (255,255,0)
+                    (r, self.objectToTrack) = cv2.CamShift(backProj, self.objectToTrack, self.termCrit)
+
+                x,y,w,h = self.objectToTrack
+                cv2.rectangle(self.frame, (x,y), (x+w,y+h), color,2)
 
             cv2.imshow("frame", self.frame)
-
             # Esperar a que se presione tecla
-            key = cv2.waitKey(1) & 0xFF
-            if ( self.face == None ):
+            if(self.face==None):
+                key = cv2.waitKey(1) & 0xFF
                 self.manuallySelectObject(key)
+            else:
+                cv2.waitKey(1) & 0xFF
+                self.faceFinding()
+                # if (len(roiPts)!=0):
+                #     self.createHist()
+
+
         camera.release()
         cv2.destroyAllWindows()
 
@@ -103,3 +112,35 @@ class Tracker:
         elif key == ord("q"):
             return quit()
 
+    def faceFinding(self):
+        global roiPts
+        orig = self.frame.copy()
+        violajones = vj.ViolaJones(self.frame, 'Video')
+        faceRects = violajones.vj()
+        # dim = (self.frame.shape[1], self.frame.shape[0]);
+        # #Aplicar Viola jones. Transformar a gris
+        # gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        # faceRects = fd.detect(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (10, 10))
+        for (x, y, w, h) in faceRects:
+            cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roiPts.append((x, y, x+w, y+h))
+            ## append points from violajones
+            #show the detected faces
+        self.createHist()
+
+    def createHist(self):
+        i = 1
+        orig = self.frame.copy()
+        for rp in roiPts:
+            i = i+1
+            # Grab the ROI for the bounding box by cropping and convert it
+            # to the HSV color space.
+            roi = orig[rp[1]:rp[-1], rp[0]:rp[2]]
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+            # compute a HSV histogram for the ROI and store the
+            # bounding box
+            rh = cv2.calcHist([roi], [0], None, [16], [0, 180])
+            rh = cv2.normalize(rh, rh, 0, 255, cv2.NORM_MINMAX)
+            self.objectToTrack = (rp[0], rp[1], rp[2], rp[-1])
+            self.roiHist=rh;
